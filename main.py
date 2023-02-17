2fef3cbdad5f4a013676cf20a6f52dfc4c4dd4a3
William Brandow <31283921+wbrandow@users.noreply.github.com>
2023-02-17 14:08:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 # Author: William Brandow
-# Date: 2023-02-09
+# Date: 2023-02-17
 
 from jupyter_plotly_dash import JupyterDash
 
 import dash
 import dash_leaflet as dl
-from dash import dcc, html, dash_table
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.express as px
-from dash.dependencies import Input, Output
+import dash_table
+from dash.dependencies import Input, Output, State
 
+import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from pymongo import MongoClient
-
+from bson.json_util import dumps
+import base64
 from animal_shelter import AnimalShelter
+
 
 ###########################
 # Data Manipulation / Model
@@ -24,17 +28,29 @@ username = 'username'
 password = 'password'
 shelter = AnimalShelter(username, password)
 
-# class read method must support return of cursor object and accept projection json input
+
+# class read method must support return of cursor object 
 df = pd.DataFrame.from_records(shelter.read())
+
 
 #########################
 # Dashboard Layout / View
 #########################
 app = JupyterDash('SimpleExample')
 
+# Grazioso Salvare’s logo
+image_filename = 'Grazioso_Salvare_Logo.png' 
+encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+
+
 app.layout = html.Div([
-    html.Div(id='hidden-div', style={'display': 'none'}),
+#    html.Div(id='hidden-div', style={'display':'none'}),
     html.Center(html.B(html.H1('SNHU CS-340 Dashboard'))),
+    html.Img(
+        src='data:image/png;base64,{}'.format(encoded_image.decode()), 
+        style={'width': '40%', 'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'}
+    ),
+    html.Center(html.B(html.H3('William Brandow'))),
     html.Hr(),
     dcc.Dropdown(
         options=[
@@ -42,10 +58,11 @@ app.layout = html.Div([
             {'label': 'Mountain Rescue', 'value': 'Mountain Rescue'},
             {'label': 'Disaster Rescue', 'value': 'Disaster Rescue'},
             {'label': 'Reset', 'value': 'Reset'}
-        ],
-        value='Reset',
+        ], 
+        value='Reset', 
         id='rescue_type'
     ),
+    html.Hr(),
     dash_table.DataTable(
         id='datatable-id',
         columns=[
@@ -53,149 +70,201 @@ app.layout = html.Div([
         ],
         data=df.to_dict('records'),
         sort_action='native',
-        sort_mode='multi',
+        sort_mode='multi',  
         filter_action='native',
         row_selectable='single',
         selected_rows=[],
         page_action='native',
-        page_current=0,
-        page_size=10,
+        page_current= 0,
+        page_size= 10,
         style_table={'overflowX': 'scroll'}
     ),
     html.Br(),
     html.Hr(),
-    html.Div(
-        className='col s12 m6',
-        children=dl.Map(
-            id='map-id',
-            style={'width': '1000px', 'height': '500px'},
-            center=[30, -97],
-            zoom=10,
-            children=[
-                dl.TileLayer(id="base-layer-id"),
-                # Marker with tool tip and popup
-                dl.Marker(
-                    id='marker-id',
-                    position=[30, -97],
-                    children=[
-                        dl.Tooltip(id='breed'),
-                        dl.Popup([
-                            html.H1("Animal Name"),
-                            html.P(id='name')
-                        ])
-                    ]
-                )
-            ]
-        )
-    )
-])
+#This sets up the dashboard so that your chart and your geolocation chart are side-by-side
+    html.Div(className='row',
+         style={'display' : 'flex'},
+             children=[
+        html.Div(
+            id='graph-id',
+            className='col s12 m6',
 
+            ),
+        html.Div(
+            id='map-id',
+            className='col s12 m6',
+            )
+        ])
+])
 
 #############################################
 # Interaction Between Components / Controller
 #############################################
 
-@app.callback(
-    [Output('breed', 'children'),
-     Output('name', 'children')],
-    [Input('datatable-id', 'derived_viewport_data'),
-     Input('datatable-id', 'selected_rows')]
-)
-def update_map(viewData, selected_rows):
-    dff = pd.DataFrame.from_dict(viewData)
-
-    # get info for selected animal
-    if selected_rows:
-        name = dff.iloc[selected_rows]['name']
-        breed = dff.iloc[selected_rows]['breed']
-        lat = dff.iloc[selected_rows]['location_lat']
-        long = dff.iloc[selected_rows]['location_long']
-    else:
-        name = dff.iloc[0]['name']
-        breed = dff.iloc[0]['breed']
-        lat = dff.iloc[0]['location_lat']
-        long = dff.iloc[0]['location_long']
-
-    center = [lat, long]
-    position = [lat, long]
-
-    return [
-        breed,
-        name
-    ]
-
-
+# Filter datatable based on rescue_type selected with dropdown
 @app.callback(
     Output('datatable-id', 'data'),
     [Input('rescue_type', 'value')]
-)
+)    
 def update_data_table(rescue_type):
     # Water Rescue selected
     if rescue_type == 'Water Rescue':
         query_result_df = pd.DataFrame.from_records(shelter.read({
-            '$and': [
-                # age between 26 and 156 weeks
-                {'$and': [
-                    {'age_upon_outcome_in_weeks': {'$gte': 26}},
+            '$and':[
+                # age must be greater than 26 AND less than 156 weeks
+                {'$and':[
+                    {'age_upon_outcome_in_weeks': {'$gte': 26}}, 
                     {'age_upon_outcome_in_weeks': {'$lte': 156}}
                 ]},
-                # preferred breeds
-                {'$or': [
-                    {'breed': 'Labrador Retriever Mix'},
-                    {'breed': 'Chesapeake Bay Retriever'},
+                # must be one of the preferred breeds
+                {'$or':[
+                    {'breed': 'Labrador Retriever Mix'}, 
+                    {'breed': 'Chesapeake Bay Retriever'}, 
                     {'breed': 'Newfoundland'}
                 ]},
-                # preferred sex
+                # must be preferred sex
                 {'sex_upon_outcome': 'Intact Female'}
             ]
         }))
-    # Mountain Rescue selected
+    # Mountain Rescue selected    
     elif rescue_type == 'Mountain Rescue':
         query_result_df = pd.DataFrame.from_records(shelter.read({
-            '$and': [
-                # age between 26 and 156 weeks
-                {'$and': [
-                    {'age_upon_outcome_in_weeks': {'$gte': 26}},
+            '$and':[
+                # age must be greater than 26 AND less than 156 weeks
+                {'$and':[
+                    {'age_upon_outcome_in_weeks': {'$gte': 26}}, 
                     {'age_upon_outcome_in_weeks': {'$lte': 156}}
                 ]},
-                # preferred breeds
-                {'$or': [
-                    {'breed': 'German Shepherd'},
-                    {'breed': 'Alaskan Malamute'},
+                # must be one of the preferred breeds
+                {'$or':[
+                    {'breed': 'German Shepherd'}, 
+                    {'breed': 'Alaskan Malamute'}, 
                     {'breed': 'Old English Sheepdog'},
                     {'breed': 'Siberian Husky'},
                     {'breed': 'Rottweiler'}
                 ]},
-                # preferred sex
+                # must be preferred sex
                 {'sex_upon_outcome': 'Intact Male'}
             ]
-        }))
-        # Disaster Rescue selected
+        }))  
+    # Disaster Rescue selected    
     elif rescue_type == 'Disaster Rescue':
         query_result_df = pd.DataFrame.from_records(shelter.read({
-            '$and': [
-                # age between 20 and 300 weeks
-                {'$and': [
-                    {'age_upon_outcome_in_weeks': {'$gte': 20}},
+            '$and':[
+                # age must be greater than 20 AND less than 300 weeks
+                {'$and':[
+                    {'age_upon_outcome_in_weeks': {'$gte': 20}}, 
                     {'age_upon_outcome_in_weeks': {'$lte': 300}}
                 ]},
-                # preferred breeds
-                {'$or': [
-                    {'breed': 'German Shepherd'},
-                    {'breed': 'Doberman Pinscher'},
+                # must be one of the preferred breeds
+                {'$or':[
+                    {'breed': 'German Shepherd'}, 
+                    {'breed': 'Doberman Pinscher'}, 
                     {'breed': 'Golden Retriever'},
                     {'breed': 'Bloodhound'},
                     {'breed': 'Rottweiler'}
                 ]},
-                # preferred sex
+                # must be preferred sex
                 {'sex_upon_outcome': 'Intact Male'}
             ]
         }))
-    # reset to non-filtered view
+    # reset to non-filtered view    
     elif rescue_type == 'Reset':
         query_result_df = pd.DataFrame.from_records(shelter.read())
-
+        
     return query_result_df.to_dict('records')
+
+
+# highlight selected row in datatable
+@app.callback(
+    Output('datatable-id', 'style_data_conditional'),
+    [Input('datatable-id', 'selected_rows')]
+)
+def update_styles(selected_rows):
+    return [{
+        'if': { 'row_index': i },
+        'background_color': '#D2F3FF'
+    } for i in selected_rows]
+
+@app.callback(
+    Output('graph-id', "children"),
+    [Input('datatable-id', "data")])
+def update_graphs(currentData):
+    dff = pd.DataFrame.from_dict(currentData)
+    df_pie = dff.loc[dff['animal_type'] == 'Dog', 'breed']
+    fig = px.pie(df_pie, names='breed', title='Dog Breeds')
+    fig.update_traces(textposition='inside')
+    fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
+    
+    return [
+        dcc.Graph(            
+            figure = fig
+        )    
+    ]
+
+
+# Display information of animal in selected row on map. If no row selected, displays row_index 1
+@app.callback(
+    Output('map-id', 'children'),
+    [Input('datatable-id', 'derived_viewport_data'),
+    Input('datatable-id', 'selected_rows')]
+)
+def update_map(viewData, selected_rows):
+    dff = pd.DataFrame.from_dict(viewData)
+        
+    # if any row is selected return info for that animal
+    if selected_rows:       
+        resulting_map = [
+            dl.Map(
+                style={'width': '1000px', 'height': '500px'}, 
+                center=[30.75,-97.48], 
+                zoom=10, 
+                children=[
+                    dl.TileLayer(id="base-layer-id"),
+                    # Marker with tool tip and popup
+                    dl.Marker(
+                        position=[
+                            dff.iloc[selected_rows[0]]['location_lat'],
+                            dff.iloc[selected_rows[0]]['location_long']
+                        ], 
+                        children=[
+                            dl.Tooltip(dff.iloc[selected_rows[0]][4]),
+                            dl.Popup([
+                                html.H1("Animal Name"),
+                                html.P(dff.iloc[selected_rows[0]][9])
+                            ])
+                        ]
+                    )
+                ]
+            )
+        ]
+    # if no row is selected return info for animal at row_index 1    
+    else:
+        resulting_map = [
+            dl.Map(
+                style={'width': '1000px', 'height': '500px'}, 
+                center=[30.75,-97.48], 
+                zoom=10, 
+                children=[
+                    dl.TileLayer(id="base-layer-id"),
+                    # Marker with tool tip and popup
+                    dl.Marker(
+                        position=[
+                            dff.iloc[1]['location_lat'],
+                            dff.iloc[1]['location_long']
+                        ], 
+                        children=[
+                            dl.Tooltip(dff.iloc[1,4]),
+                            dl.Popup([
+                                html.H1("Animal Name"),
+                                html.P(dff.iloc[1,9])
+                            ])
+                        ]
+                    )
+                ]
+            )
+        ]        
+    return resulting_map
 
 
 app
